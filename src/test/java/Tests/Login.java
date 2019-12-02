79c1e0b0018557d558b16aa5f4b412a72d60683c
Sebastian <greekpsych@yahoo.co.uk>
2019-12-02 20:19:43 +0000

    updated 2 files

--- src/test/java/Tests/Login.java
+++ src/test/java/Tests/Login.java
@@ -1,4 +1,11 @@
 package Tests;
 
+import sun.jvm.hotspot.HelloWorld;
+
 public class Login {
+
+    public static void main(String[] args) {
+
+
+    }
 }
--- src/test/java/Tests/Logout.java
+++ src/test/java/Tests/Logout.java
@@ -2,5 +2,8 @@ package Tests;
 
 public class Logout {
 
+    public static void main(String[] args) {
 
+        //this is something new
+    }
 }
