745b9f08c5ccc7a48512a82b18fbdb1f486dc375
tasyseby <57195437+tasyseby@users.noreply.github.com>
2019-12-09 17:37:44 +0000

    updated task class

--- src/test/java/Tests/Task.java
+++ src/test/java/Tests/Task.java
@@ -2,5 +2,9 @@ package Tests;
 
 public class Task {
 
-
+    public static void main (String [] args){
+    System.out.println("Hello World");
+    //this line is added from Github see you in Intellijy
+    
+    }
 }
