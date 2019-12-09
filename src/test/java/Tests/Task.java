2e2a42073c49c2f45d26285caaed64e28e8a318b
Sebastian <greekpsych@yahoo.co.uk>
2019-12-09 18:15:47 +0000

    add void method

--- src/test/java/Tests/Task.java
+++ src/test/java/Tests/Task.java
@@ -5,6 +5,10 @@ public class Task {
     public static void main (String [] args){
     System.out.println("Hello World");
     //this line is added from Github see you in Intellijy
-    
     }
+
+    public void hi(){
+        System.out.println("Hi from itellijy");
+    }
+
 }
